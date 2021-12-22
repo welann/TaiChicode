ce452d52cfbfc412fbe88f645b2f8836d266c6de
welann <wzcing@outlook.com>
2021-12-22 20:12:51 +0800

    第二课

--- taichiCourse/hitaichi.py
+++ taichiCourse/hitaichi.py
@@ -1,28 +1,76 @@
 import taichi as ti
 
-ti.init(arch=ti.gpu)
+ti.init(ti.cpu)
 
-n = 320
-pixels = ti.field(dtype=float, shape=(n * 2, n))
+paused=ti.field(ti.i32,())
 
-@ti.func
-def complex_sqr(z):
-    return ti.Vector([z[0]**2 - z[1]**2, z[1] * z[0] * 2])
+G=100
+PI=3.141592653
+
+N=1000
+
+m=59
+
+galaxy_size=0.4
+
+planet_radius=3
+
+init_vel=1000
+
+h=1e-6
+
+substepping=10
+
+
+pos=ti.Vector.field(2,ti.f32,N)
+vel=ti.Vector.field(2,ti.f32,N)
+force=ti.Vector.field(2,ti.f32,N)
 
 @ti.kernel
-def paint(t: float):
-    for i, j in pixels:  # Parallelized over all pixels
-        c = ti.Vector([-0.8, ti.cos(t) * 0.2])
-        z = ti.Vector([i / n - 1, j / n - 0.5]) * 2
-        iterations = 0
-        while z.norm() < 20 and iterations < 50:
-            z = complex_sqr(z) + c
-            iterations += 1
-        pixels[i, j] = 1 - iterations * 0.02
+def initialize():
+    center=ti.Vector([0.5,0.5])
+    for i in range(N):
+        theta=ti.random()*2*PI
+        r=(ti.sqrt(ti.random())*0.7+0.3)*galaxy_size
+        offset=r*ti.Vector([ti.cos(theta),ti.sin(theta)])
+        pos[i]=center+offset
+        vel[i]=[-offset.y,offset.x]
+        vel[i]*=init_vel
 
-gui = ti.GUI("Julia Set", res=(n * 2, n))
 
-for i in range(1000000):
-    paint(i * 0.03)
-    gui.set_image(pixels)
+@ti.kernel
+def compute_force():
+    for i in range(N):
+        force[i]=ti.Vector([0.0,0.0])
+    
+    for i in range(N):
+        p=pos[i]
+        for j in range(i):
+            diff=p-pos[j]
+            r=diff.norm(1e-5)
+
+            f=-G*m*m*(1.0/r)**3*diff
+            force[i]+=f
+            force[j]+=-f
+
+
+
+@ti.kernel
+def update():
+    dt=h/substepping
+    for i in range(N):
+        vel[i]+=dt*force[i]/m
+        pos[i]+=dt*vel[i]
+
+gui=ti.GUI("N-body problem",(512,512))
+
+initialize()
+while gui.running:
+    for i in range(substepping):
+        compute_force()
+        update()
+    
+    gui.clear(0x112f41)
+    gui.circles(pos.to_numpy(),color=0xffffff,radius=planet_radius)
     gui.show()
+
